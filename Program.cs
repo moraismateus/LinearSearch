c8053b97fcedcbf8e754e70ea0df107a6f8d81dd
Mateus Morais <mateus87morais@gmail.com>
2022-08-30 18:16:28 -0700

    Initial

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿
+var myArray = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+var target = 7;
+
+Console.WriteLine(LinearSearch(myArray, target));
+
+int LinearSearch(int[] array, int target)
+{
+    for (int index = 0; index < array.Length; index++)
+    {
+        if (array[index] == target)
+            return index;
+    }
+
+    return -1;
+}
+
